ae248f46ae5f47722df820d215dd0dc047f51e65
IUSER\611802116 <Mahdi.jubayer@bt.com>
2020-08-17 17:36:25 +0100

    Updates

---
+++ precache-manifest.6ec17273a7ea362365e097b9fff524ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe0d18eb4d3a06d933bb8c3517fa2a9",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b78b7272cb2511f0398c",
+    "url": "/monster-rolodex/static/css/main.e2b333b5.chunk.css"
+  },
+  {
+    "revision": "db8d097646ea2c7433d0",
+    "url": "/monster-rolodex/static/js/2.6e94c425.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.6e94c425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78b7272cb2511f0398c",
+    "url": "/monster-rolodex/static/js/main.26d4b790.chunk.js"
+  },
+  {
+    "revision": "04c0610ce6fe6a01ce65",
+    "url": "/monster-rolodex/static/js/runtime-main.9063aeb6.js"
+  }
+]);
